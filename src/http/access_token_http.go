15d9d5c93f1b25bf4f598984a28775796cf4fa37
Narachii <naoya706oxford@gmail.com>
2021-08-05 11:21:47 +0900

    implemnt cassandra handling

--- src/http/access_token_http.go
+++ src/http/access_token_http.go
@@ -2,12 +2,14 @@ package http
 
 import (
 	"github.com/Narachii/bookstore_oauth_api/src/domain/access_token"
+	"github.com/Narachii/bookstore_oauth_api/src/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 type AccessTokenHandler interface {
 	GetById(*gin.Context)
+	Create(c *gin.Context)
 }
 
 type accessTokenHandler struct {
@@ -19,7 +21,7 @@ func NewHandler(service access_token.Service) AccessTokenHandler {
 		service: service,
 	}
 }
-func (h accessTokenHandler) GetById(c *gin.Context) {
+func (h *accessTokenHandler) GetById(c *gin.Context) {
 	accessTokenId := c.Param("access_token_id")
 	accessToken, err := h.service.GetById(accessTokenId)
 	if err != nil {
@@ -27,5 +29,19 @@ func (h accessTokenHandler) GetById(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusNotImplemented, accessToken)
+	c.JSON(http.StatusOK, accessToken)
+}
+func (h *accessTokenHandler) Create(c *gin.Context) {
+	var at access_token.AccessToken
+	if err := c.ShouldBindJSON(&at); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	if err := h.service.Create(at); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusCreated, at)
 }
