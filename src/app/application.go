9de91501adff9f8d368ed09e2954769dd70d4b8d
Narachii <naoya706oxford@gmail.com>
2021-08-14 00:20:37 +0900

    removed session creation from application.go (#5)

--- src/app/application.go
+++ src/app/application.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"github.com/Narachii/bookstore_oauth_api/src/clients/cassandra"
 	"github.com/Narachii/bookstore_oauth_api/src/domain/access_token"
 	"github.com/Narachii/bookstore_oauth_api/src/http"
 	"github.com/Narachii/bookstore_oauth_api/src/repository/db"
@@ -13,8 +12,6 @@ var (
 )
 
 func StartApplication() {
-	session := cassandra.GetSession()
-	session.Close()
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
 
 	router.GET("/oauth/access_token/:access_token_id", atHandler.GetById)
